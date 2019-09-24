60d6d189bf9ba77e66ba97b0acd72e3cebb366bc
Nathan <Nathan.dalessio@snhu.edu>
2019-09-24 13:21:27 -0400

    update

---
+++ Assets/Nate/CelebVisible.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CelebVisible : MonoBehaviour
+{
+    public bool isVisibile = false;
+
+    private void OnBecameVisible()
+    {
+        isVisibile = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        isVisibile = false;
+    }
+}
--- Assets/Nate/SnapShot.cs
+++ Assets/Nate/SnapShot.cs
@@ -9,6 +9,19 @@ public class SnapShot : MonoBehaviour
 
     private bool takeHiResShot = false;
 
+    GameObject celeb;
+
+    bool m_Started;
+    public LayerMask m_LayerMask;
+
+    public Vector3 collider;
+
+    void Start()
+    {
+        //Use this to ensure that the Gizmos are being drawn when in Play Mode.
+        m_Started = true;
+    }
+
     public static string ScreenShotName(int width, int height)
     {
         return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
@@ -22,11 +35,59 @@ public class SnapShot : MonoBehaviour
         takeHiResShot = true;
     }
 
+    private void Update()
+    {
+
+    }
+
+    public bool MyCollisions()
+    {
+        Collider[] hitColliders = Physics.OverlapBox(new Vector3(transform.position.x, transform.position.y, transform.position.z + (collider.z / 2)), collider, Quaternion.identity, m_LayerMask);
+        int i = 0;
+        //Check when there is a new collider coming into contact with the box
+        while (i < hitColliders.Length)
+        {
+            //Output all of the collider names
+            Debug.Log("Hit : " + hitColliders[i].name + i);
+            //Increase the number of Colliders in the array
+            i++;
+        }
+
+        foreach(Collider c in hitColliders)
+        {
+            if (c.GetComponent<CelebVisible>() != null)
+            {
+                if (c.GetComponent<CelebVisible>().isVisibile == true)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
+        if (m_Started)
+            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
+            Gizmos.DrawWireCube(new Vector3(transform.position.x, transform.position.y, transform.position.z + (collider.z / 2)), collider);
+    }
+
     void LateUpdate()
     {
         takeHiResShot |= Input.GetKeyDown("k");
         if (takeHiResShot)
         {
+            // is the celeb in the shot
+            if (MyCollisions() == true)
+            {
+                Debug.Log("Celeb Captured");
+            }
+            // else if () add another function to look for past celebs 
+
             RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
             GetComponent<Camera>().targetTexture = rt;
 
