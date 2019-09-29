df0572e5c3f3f11ac063886d9c248f4459f7cab7
mwoods19 <mathew.woods@snhu.edu>
2019-09-29 18:03:09 -0400

    can load in picture.

---
+++ Assets/Mathew/LoadImage.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadImage : MonoBehaviour
+{
+    [SerializeField]
+    RawImage image;
+
+    Texture screenShot;
+
+    [SerializeField]
+    Transform trans;
+
+    bool done = false;
+
+    public void Start()
+    {
+        screenShot = Resources.Load<Texture>("screen");
+
+        image.GetComponent<RawImage>().texture = screenShot;
+
+        Debug.Log("Image Position: " + image.rectTransform.localPosition);
+    }
+        
+
+    private void Update()
+    {
+        if (done == false)
+        {
+            Vector3 newPos = image.rectTransform.localPosition;
+
+            newPos.x += 100;
+
+            for (int i = 1; i < 4; ++i)
+            {
+                Debug.Log(newPos);
+
+                Instantiate(image, trans);
+
+                image.rectTransform.localPosition += newPos;
+
+                Debug.Log(i);
+            }
+            done = true;
+        }
+    }
+}
