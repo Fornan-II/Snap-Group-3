5b45221d88c513865caf209306185859255fe3df
mwoods19 <mathew.woods@snhu.edu>
2019-09-30 13:01:53 -0400

    working picture load

--- Assets/Mathew/LoadImage.cs
+++ Assets/Mathew/LoadImage.cs
@@ -11,9 +11,6 @@ public class LoadImage : MonoBehaviour
     Texture screenShot;
 
     [SerializeField]
-    Transform trans;
-
-    [SerializeField]
     List<RawImage> rawImages;
 
     bool done = false;    
@@ -24,19 +21,11 @@ public class LoadImage : MonoBehaviour
         {
             Vector3 newPos = image.rectTransform.localPosition;
 
-            newPos.x += 100;
-
-            for (int i = 1; i < rawImages.Count; ++i)
+            for (int i = 0; i < rawImages.Count; ++i)
             {
-                Debug.Log(i);
-
                 screenShot = Resources.Load<Texture>("screen " + i);
 
-                Debug.Log(screenShot);
-
-                image.GetComponent<RawImage>().texture = screenShot;
-
-                Debug.Log(image.GetComponent<RawImage>().texture = screenShot);
+                rawImages[i].GetComponent<RawImage>().texture = screenShot;
             }
             done = true;
         }
