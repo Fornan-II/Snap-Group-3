e998b484a9a708cd87bbaef6a95fa6a9b7e2f55b
Fornan-II <37092360+Fornan-II@users.noreply.github.com>
2019-10-03 12:23:24 -0400

    Fixed build error

--- Assets/Maxen/DEBUG_CharSpawner.cs
+++ Assets/Maxen/DEBUG_CharSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿#if UNITY_EDITOR
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -161,3 +162,4 @@ public class DEBUG_CharSpawner : MonoBehaviour
         oldPart = ShowPart;
     }
 }
+#endif
